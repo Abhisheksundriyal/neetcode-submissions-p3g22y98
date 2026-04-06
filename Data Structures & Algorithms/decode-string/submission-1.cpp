82397af02646d3e3a3460c432ececf4c1d4fa37b
Abhishek Sundriyal <111180159+Abhisheksundriyal@users.noreply.github.com>
2026-04-06 12:02:28 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<char> st;
+        int n=s.size();
+        string ans;
+        for(int i=0;i<n;i++) {
+            if(s[i]!=']') st.push(s[i]);
+            else {
+                string sub;
+                while(st.top()!='[') {
+                    sub.push_back(st.top());
+                    st.pop();
+                }
+                st.pop();
+                reverse(sub.begin(),sub.end());
+                string num;
+                while(!st.empty() && st.top()>='0' && st.top()<='9') {
+                    num.push_back(st.top());
+                    st.pop();
+                }
+                reverse(num.begin(),num.end());
+                int x=stoi(num);
+                for(int j=0;j<x;j++) {
+                    for(int k=0;k<sub.size();k++) st.push(sub[k]);
+                }
+            }
+        }
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
