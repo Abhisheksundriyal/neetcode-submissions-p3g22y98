10453b4635dd52f99f349f1d03117e9f7d757779
Abhishek Sundriyal <111180159+Abhisheksundriyal@users.noreply.github.com>
2026-07-07 15:37:20 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const st = new Set();
+        for(let i=0;i<nums.length;i++) {
+            if(st.has(nums[i])) return true;
+            st.add(nums[i]);
+        }
+        return false;
+    }
+}
